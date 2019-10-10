1d8a351069598c6424574d5a9914656450a61ed0
Benny <Benjamin.grill@ue-germany.de>
2019-10-10 11:56:17 +0200

    LockOn
    Player now can Lock on to Boss

--- PrototypingBase/Assets/Prototype/Scripts/Camera/FreeLookCamera.cs
+++ PrototypingBase/Assets/Prototype/Scripts/Camera/FreeLookCamera.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FreeLookCamera : MonoBehaviour
 {
     [SerializeField]
     private Transform playerTarget;
-    //[SerializeField]
+    [SerializeField]
     private Transform enemyTarget;
     private Transform pivot;
     private Transform cam;
@@ -25,6 +23,7 @@ public class FreeLookCamera : MonoBehaviour
     private float offSetZ;
     [SerializeField]
     private float followSpeed;
+
     [Header("Camera movement")]
     [Range(1, 10)]
     [SerializeField]
@@ -36,22 +35,32 @@ public class FreeLookCamera : MonoBehaviour
     [SerializeField]
     private float maxAngle;
 
+    [Header("Camera LockOn")]
+    [Range(0, 1)]
+    [SerializeField]
+    private float lookAtSpeed;
+    [Range(0, 10)]
+    [SerializeField]
+    private float lockOnOffSetY;
+
     private float rotationX;
     private float rotationY;
 
     private Vector3 offSet;
     private Vector3 offSetNew;
     private Vector3 offSetOld;
-    private Vector3 lookAt;
 
+    [HideInInspector]
+    public Vector3 lookAt;
 
-    private bool lockOn = false;
+    [HideInInspector]
+    public bool lockOn = false;
 
     private void Start()
     {
         offSetNew = new Vector3(0, offSetY, offSetZ);
         cam = Camera.main.transform;
-        offSetOld = offSetNew;
+        offSetOld = new Vector3(0, 0, 0);
         pivot = cam.parent;
     }
 
@@ -59,12 +68,22 @@ public class FreeLookCamera : MonoBehaviour
     {
         CameraMove();
         CameraRotation();
+        if (Input.GetButtonDown("CameraButton"))
+        {
+            lockOn = !lockOn;
+        }
     }
 
     private void FixedUpdate()
     {
-        offSetNew = new Vector3(0, offSetY, offSetZ);
-
+        if (!lockOn)
+        {
+            offSetNew = new Vector3(0, offSetY, offSetZ);
+        }
+        else
+        {
+            offSet = new Vector3(0, lockOnOffSetY, offSetZ);
+        }
         if (offSetOld != offSetNew)
         {
             cam.localPosition = new Vector3(pivot.localPosition.x, pivot.localPosition.y + offSetY, pivot.localPosition.z + offSetZ);
@@ -74,30 +93,38 @@ public class FreeLookCamera : MonoBehaviour
 
     public void CameraMove()
     {
-            Vector3 targetPosition = Vector3.Lerp(transform.position, playerTarget.transform.position, followSpeed);
-            transform.position = targetPosition;
+        Vector3 targetPosition = Vector3.Lerp(transform.position, playerTarget.transform.position, followSpeed);
+        transform.position = targetPosition;
     }
 
     private void CameraRotation()
     {
-        if (Input.GetAxisRaw("CameraHorizontal") > deadZone ||
+        if ((Input.GetAxisRaw("CameraHorizontal") > deadZone ||
             Input.GetAxisRaw("CameraHorizontal") < -deadZone ||
             Input.GetAxisRaw("CameraVertical") > deadZone ||
-            Input.GetAxisRaw("CameraVertical") < -deadZone)
+            Input.GetAxisRaw("CameraVertical") < -deadZone) && !lockOn)
         {
 
             rotationY -= Input.GetAxisRaw("CameraVertical") * senitivity;
             rotationY = Mathf.Clamp(rotationY, minAngle, maxAngle);
             pivot.localRotation = Quaternion.Euler(rotationY, 0, 0);
 
-            if(lockOn)
-            {
-                lookAt = enemyTarget.position;
-            }
-
             rotationX += Input.GetAxisRaw("CameraHorizontal") * senitivity;
             transform.rotation = Quaternion.Euler(0, rotationX, 0);
+        }
+        else if (lockOn && enemyTarget != null)
+        {
+            lookAt = enemyTarget.position - transform.position;
+            lookAt.Normalize();
+            //lookAt.y = 0
 
+            if (lookAt == Vector3.zero)
+            {
+                lookAt = transform.forward;
+            }
+            Quaternion lookAtRot = Quaternion.LookRotation(lookAt);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookAtRot, lookAtSpeed);
+            rotationX = lookAt.x;
         }
     }
 }
