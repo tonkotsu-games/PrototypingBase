1c2b3e7ef9355178eebeff06e4e01fa55267b079
Benjamin Grill <benjamin.grill@ue-germany.de>
2019-10-19 18:48:43 +0200

    Player Controller
    Added designer Stances
    added some animation states
    some bug Fixes
    added a Debug mode
    made some pre assignments against the warnings

---
+++ PrototypingBase/Assets/Prototype/Scripts/Player/Weapons/Gun/FiringGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringGun : MonoBehaviour
+{
+    [Header("Gun Point Gameobjects")]
+    [SerializeField]
+    private GameObject gunPoint = null;
+    [SerializeField]
+    private GameObject bullet = null;
+    private GameObject instanceBullet = null;
+
+    [SerializeField]
+    private float bulletSpeed = 0;
+
+    public void Shoot()
+    {
+        instanceBullet = Instantiate(bullet, gunPoint.transform.position, Quaternion.LookRotation(transform.forward));
+        instanceBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+    }
+
+}
