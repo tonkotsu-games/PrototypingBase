e67e3cd8005bd04aef86d0d5fd80c0e022092ab9
Niklas <niklashassmann@tonkotsu.games>
2019-10-23 14:47:27 +0200

    Added NavmeshComponents + TMP Essentials. Mockup Spawners and Wave Management done
    add WaveManager to scene for spawners to work

---
+++ PrototypingBase/Assets/Prototype/Scripts/NiklasProto/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField][Tooltip("The Wave this Spawner activates")]
+    public Wave.Waves Wave;
+    [SerializeField]
+    public int amountOfEnemiesToSpawn;
+    [SerializeField]
+    GameObject mockupEnemyPrefab;
+
+    private bool doneSpawning = false;
+
+    private void Start()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+    private void Update()
+    {
+        if(WaveManager.instance.currentWaveState == Wave && !doneSpawning)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        for (int i = 0; i < amountOfEnemiesToSpawn; i++)
+        {
+            Instantiate(mockupEnemyPrefab, transform.position, Quaternion.identity);
+        }
+        doneSpawning = true;
+    }
+}
