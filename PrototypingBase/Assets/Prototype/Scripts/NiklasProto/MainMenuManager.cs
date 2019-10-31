2c4f2c09b5001d168aa0d808dfc6770dea7e1692
Niklas <niklashassmann@tonkotsu.games>
2019-10-31 14:40:13 +0100

    Full Game Loop now working

---
+++ PrototypingBase/Assets/Prototype/Scripts/NiklasProto/MainMenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private GameObject previousPanel;
+    private GameObject currentPanel;
+
+    [SerializeField]
+    private GameObject mainPanel;
+
+    [SerializeField]
+    private GameObject characterPanel;
+
+    [SerializeField]
+    private GameObject optionsPanel;
+
+
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(2);      
+    }
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnCharacterClick()
+    {
+        previousPanel = mainPanel;
+        mainPanel.SetActive(false);
+        characterPanel.SetActive(true);
+        currentPanel = characterPanel;
+    }
+
+    public void OnOptionsClick()
+    {
+        previousPanel = mainPanel;
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+        currentPanel = optionsPanel;
+
+    }
+    
+    public void OnBackClick()
+    {
+        previousPanel.SetActive(true);
+        currentPanel.SetActive(false);
+        GameObject temp = previousPanel;
+        previousPanel = currentPanel;
+        currentPanel = temp;        
+    }
+}
