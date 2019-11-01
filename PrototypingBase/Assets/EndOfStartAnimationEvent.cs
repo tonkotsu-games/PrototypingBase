917753a3de516587eb35e60ea36ab4dbea44e797
Niklas <niklashassmann@tonkotsu.games>
2019-11-01 11:17:27 +0100

    player locked on elevator

--- PrototypingBase/Assets/EndOfStartAnimationEvent.cs
+++ PrototypingBase/Assets/EndOfStartAnimationEvent.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class EndOfStartAnimationEvent : MonoBehaviour
 {
+    private PlayerController playerScript;
+
+    private void Start()
+    {
+        playerScript = Locator.instance.GetPlayerGameObject().GetComponent<PlayerController>();
+    }
     public void AnimationOver()
     {
         WaveManager.instance.waveSpawnPaused = false;
+    }
+
+    public void LockPlayer()
+    {
+        playerScript.enabled = false;
+    }
+
+    public void UnlockPlayer()
+    {
+        playerScript.enabled = true;
     }
 }
