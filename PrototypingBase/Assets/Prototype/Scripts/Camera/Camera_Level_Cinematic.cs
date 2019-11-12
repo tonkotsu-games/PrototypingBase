1700c57e316e451cbc4c14ca54f0c3f9344659af
Alex <alexander.v.anhalt@gmail.com>
2019-11-12 13:22:44 +0100

    Killing myself because i have 2 code

---
+++ PrototypingBase/Assets/Prototype/Scripts/Camera/Camera_Level_Cinematic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Level_Cinematic : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            player.GetComponent<PlayerNiklas>().enabled = false;
+        }
+        else
+        {
+            player.GetComponent<PlayerNiklas>().enabled = true;           
+        }
+    }
+}
---
+++ PrototypingBase/Assets/Prototype/Scripts/Camera_Playmyani.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Playmyani : MonoBehaviour
+{
+    
+    [SerializeField] private Animator Camera_Level_Animation;
+    [SerializeField] GameObject Camera2_Level_Animation;
+    [SerializeField] GameObject Camera;
+    GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+   {
+        if(other.CompareTag("Player"))
+        {
+            Camera_Level_Animation.SetBool("Camera_Level_Cinematic", true);
+            player.GetComponent<PlayerNiklas>().enabled = false;
+            Camera2_Level_Animation.GetComponent<Camera2_Level_Animation>().enabled = true;
+            Camera.GetComponent<Camera>().enabled = false;
+        }
+        else
+        {
+            player.GetComponent<PlayerNiklas>().enabled = true;
+            GetComponent<Camera_Level_Cinematic>().enabled = false;
+        }
+
+    }
+}
